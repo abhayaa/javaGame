5a38eaa653611f585e7d37b6d7834a17463b8952
Abhaya Basnet <abhayabasnet@Swag.local>
2018-08-21 22:24:37 -0500

    finished frame, added some methods to game and created two new classes

--- src/com/game/main/Game.java
+++ src/com/game/main/Game.java
@@ -1,10 +1,93 @@
 package com.game.main;
 
-public class Game {
+import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.image.BufferStrategy;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+public class Game extends Canvas implements Runnable{
+	
+	public static final int WIDTH = 640, HEIGHT = WIDTH / 12 * 9;
+	
+	
+	private static final long serialVersionUID = -473349850293143017L;
+	private Thread thread;
+	private boolean running = false; 
+	
+	
+	public Game(){
+		new Frame(WIDTH, HEIGHT, "Game Title", this);
+		
 	}
 
+	public synchronized void start(){
+		thread = new Thread(this);
+		thread.start();
+		running = true; 
+	}
+	
+	public synchronized void stop(){
+		try {
+			thread.join();
+			running = false;
+		}catch(Exception e){
+			e.printStackTrace();	
+		}
+	}
+	
+	public void run(){
+		long lastTime = System.nanoTime();
+		double amountOfTicks = 60.0;
+		double ns = 1000000000 / amountOfTicks;
+		double delta = 0;
+		long timer = System.currentTimeMillis();
+		int frames = 0;
+		while(running){
+			long now = System.nanoTime();
+			delta += (now - lastTime) / ns;
+			lastTime = now; 
+			while(delta >= 1){
+				tick();
+				delta--;
+			}
+			if(running){
+				render();
+			}
+			frames++;
+			
+			if(System.currentTimeMillis() - timer > 1000){
+				timer += 1000;
+				System.out.println("FPS: " + frames);
+				frames = 0;
+			}
+			
+		}
+		stop();
+	}
+	
+	private void tick(){
+		
+	}
+	
+	private void render(){
+		BufferStrategy buff = this.getBufferStrategy();
+		if(buff == null){
+			this.createBufferStrategy(3);
+			return;
+		}
+		
+		Graphics g = buff.getDrawGraphics();
+		g.setColor(Color.black);
+		g.fillRect(0, 0, WIDTH, HEIGHT);
+		g.dispose();
+		buff.show();
+	}
+	
+	
+	public static void main(String[] args) {
+		new Game();
+
+	}
 }
+
+
