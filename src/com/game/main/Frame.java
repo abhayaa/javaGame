f344f96ce987e0d175950c48a7fd7a9b9e9d72ed
Abhaya Basnet <abhayabasnet@Swag.local>
2018-08-21 22:23:45 -0500

    finished frame

--- src/com/game/main/Frame.java
+++ src/com/game/main/Frame.java
@@ -1,5 +1,29 @@
 package com.game.main;
 
-public class Frame {
+import java.awt.Canvas;
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+
+public class Frame extends Canvas{
+
+	private static final long serialVersionUID = 2618658522386099128L;
+
+
+	public Frame(int width, int height, String title, Game game){
+		JFrame window = new JFrame(title); 
+		
+		window.setPreferredSize(new Dimension(width, height));
+		window.setMaximumSize(new Dimension(width, height));
+		window.setMinimumSize(new Dimension(width, height));
+		
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setResizable(false);
+		window.setLocationRelativeTo(null);
+		window.add(game);
+		window.setVisible(true);
+		game.start();
+		
+	}
 
 }
